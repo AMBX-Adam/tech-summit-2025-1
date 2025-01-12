62717abc07db457d624d1fdeb783d2557fdb68cf
AdamTan <tanadam124@gmail.com>
2025-01-12 19:49:36 +0800

    feat: Added TSGameplayTags with a line that will crash the game in a packaged build and will not generate any logs

---
+++ Source/TechSummit2025/TSGameplayTags.cpp
@@ -0,0 +1,45 @@
+#include "TSGameplayTags.h"
+#include "GameplayTagsManager.h"
+
+FTSGameplayTags FTSGameplayTags::GameplayTags;
+
+void FTSGameplayTags::InitializeNativeTags()
+{
+	UGameplayTagsManager& Manager = UGameplayTagsManager::Get();
+
+	Manager.AddTagIniSearchPath(FPaths::ProjectConfigDir() / TEXT("GameplayTags"));
+
+	GameplayTags.AddTags();
+
+	// Notify manager that we are done adding native tags.
+	Manager.DoneAddingNativeTags();
+}
+
+FGameplayTag FTSGameplayTags::FindTagByString(FString TagString, bool bMatchPartialString)
+{
+	const UGameplayTagsManager& Manager = UGameplayTagsManager::Get();
+	FGameplayTag Tag = Manager.RequestGameplayTag(FName(*TagString), false);
+
+	if (!Tag.IsValid() && bMatchPartialString)
+	{
+		FGameplayTagContainer AllTags;
+		Manager.RequestAllGameplayTags(AllTags, true);
+
+		for (const FGameplayTag TestTag : AllTags)
+		{
+			if (TestTag.ToString().Contains(TagString))
+			{
+				UE_LOG(LogTemp, Display, TEXT("Could not find exact match for tag [%s] but found partial match on tag [%s]."), *TagString, *TestTag.ToString());
+				Tag = TestTag;
+				break;
+			}
+		}
+	}
+
+	return Tag;
+}
+
+void FTSGameplayTags::AddTag(FGameplayTag& OutTag, const ANSICHAR* TagName, const ANSICHAR* TagComment)
+{
+	OutTag = UGameplayTagsManager::Get().AddNativeGameplayTag(FName(TagName), FString(TEXT("(Native) ")) + FString(TagComment));
+}
---
+++ Source/TechSummit2025/TSGameplayTags.h
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "GameplayTagsManager.h"
+#include "UObject/Object.h"
+
+/**
+ * 
+ */
+
+struct TECHSUMMIT2025_API FTSGameplayTags : public FGameplayTagNativeAdder
+{
+public:
+	FGameplayTag TestTag;
+	
+protected:
+	FTSGameplayTags()
+	{
+		UGameplayTagsManager& Manager = UGameplayTagsManager::Get();
+
+		//This crashes the game and it will not run
+		Manager.AddTagIniSearchPath(FPaths::ProjectConfigDir() / TEXT("GameplayTags"));
+	}
+	
+	virtual void AddTags() override
+	{
+		AddTag(TestTag, "TestTag", "TestTag for testing");
+	}
+
+	//========================================
+	// Internal functions
+	//========================================
+public:
+	static const FTSGameplayTags& Get() { return GameplayTags; }
+	static void InitializeNativeTags();
+	static FGameplayTag FindTagByString(FString TagString, bool bMatchPartialString = false);
+
+protected:
+	// @see FLyraGameplayTags
+	void AddTag(FGameplayTag& OutTag, const ANSICHAR* TagName, const ANSICHAR* TagComment);
+
+private:
+	static FTSGameplayTags GameplayTags;
+};
